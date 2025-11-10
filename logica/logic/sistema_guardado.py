40571380258ae547994f5db901abd6e7808f7f95
Guatibal <oscaranibal2711@gmail.com>
2025-11-09 22:15:27 -0300

    Implement SistemaGuardado class for simulation saving

---
+++ logica/logic/sistema_guardado.py
@@ -0,0 +1,17 @@
+from logic.estado_simulacion import EstadoSimulacion
+
+class SistemaGuardado:
+    
+    def __init__(self, ruta_guardado="data/simulacion.csv"):
+        self.ruta_guardado = ruta_guardado
+
+    def guardar_simulacion(self, estado: EstadoSimulacion):
+        # Requisito: Debe existir la función.
+        # Se implementará en el Entregable 3.
+        pass
+
+    def cargar_simulacion(self, ruta_archivo: str) -> EstadoSimulacion:
+        # Requisito: Debe existir la función.
+        # Se implementará en el Entregable 3.
+        pass
+#en construccion
